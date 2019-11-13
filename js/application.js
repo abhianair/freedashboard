2254a4f86267d167459dedde06a3a639e6513b3f
Abhishek Aravindan <abhishek@flyingloop.com>
2019-11-13 15:29:07 +0530

    aria controlled tabs

--- js/application.js
+++ js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   console.log('Free DashBoard Style');
+
+  $("li[role='tab']").click(function(){
+  	$("li[role='tab']").attr("aria-selected","false"); //deselect all the tabs
+   	$(this).attr("aria-selected","true");  // select this tab
+  	var tabpanid= $(this).attr("aria-controls"); //find out what tab panel this tab controls
+     var tabpan = $("#"+tabpanid);
+  	$("div[role='tabpanel']").attr("aria-hidden","true"); //hide all the panels
+  	tabpan.attr("aria-hidden","false");  // show our panel
+   });
+
+
 });
